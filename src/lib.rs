bbf9d79015f08dfd7305896bb051b02dade94108
Nerothos <nerothos@live.fr>
2020-05-31 20:23:16 +0200

    Add region parameter (#1)
    * Add region parameter
    
    * Update Cargo.toml
    
    * Fixing cn url
    
    * Revert "Update Cargo.toml"
    
    This reverts commit 377b848d211b7ce850322b395f31ac39a64df7b7.

--- src/lib.rs
+++ src/lib.rs
@@ -7,19 +7,25 @@ pub struct OAuthToken {
     pub expires_in: usize,
 }
 
-/// To retrieve a token, you need to provide your client_id and client_secret
+/// To retrieve a token, you need to provide your client_id and client_secret as well as a region (US, EU, APAC or CN)
 ///
 /// ```rust
-/// let token = battle_net_oauth::get_oauth_token("client_id", "client_secret");
+/// let token = battle_net_oauth::get_oauth_token("client_id", "client_secret", "region");
 /// ```
 pub async fn get_oauth_token(
     client_id: &str,
     client_secret: &str,
+    region: &str,
 ) -> Result<OAuthToken, Box<dyn std::error::Error>> {
     let client = reqwest::Client::new();
+    let url = if region.to_lowercase() == "cn" {
+        "https://www.battlenet.com.cn/oauth/token".to_string()
+    } else {
+        format!("https://{}.battle.net/oauth/token", region.to_lowercase())
+    };
 
     let resp: OAuthToken = client
-        .post("https://eu.battle.net/oauth/token")
+        .post(&url)
         .basic_auth(client_id, Some(client_secret))
         .form(&[("grant_type", "client_credentials")])
         .send()
